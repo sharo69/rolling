02de1f744809117bb6ac57319d25d204a4a3c708
nikola.kaloyanov <nikola.kaloyanov@trading212.com>
2016-02-24 15:05:06 +0200

    sdfds

--- src/main/java/com/rolling/HelloController.java
+++ src/main/java/com/rolling/HelloController.java
@@ -1,10 +1,12 @@
 package com.rolling;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by nikola.kaloyanov on 2/24/2016.
  */
+@RestController
 public class HelloController
 {
 	@RequestMapping("/hello")
